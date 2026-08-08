e42cbe9f22fff1a2a598505725f284618ebc3e4c
Neeraj <ny855768@gmail.com>
2026-08-08 11:33:11 +0530

    aaded one rg

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-resource"azurerm_resource_group""rgs"{
-name="rg_name"
-location="centralindia"
+resource "azurerm_resource_group" "rgs" {
+  name     = "rg_name"
+  location = "centralindia"
+}
+
+resource "azurerm_resource_group" "rgs1" {
+  name     = "rg_name1"
+  location = "centralindia"
 }
 
